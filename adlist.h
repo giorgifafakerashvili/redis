bd44a296acf92c0018b8387b1720d424304e36d5
= <giorgi.fafa@gmail.com>
2017-06-16 02:35:57 +0400

    adlist.h partially

---
+++ adlist.h
@@ -0,0 +1,53 @@
+#ifndef _ADLIST_H_
+#define _ADLIST_H_
+
+/**
+ * Node, List and Iterator are the only data structures used currently
+ */
+typedef struct listNode {
+  struct listNode* prev;
+  struct listNode* next;
+  void* value;
+} listNode;
+
+
+typedef struct listIter {
+  listNode* next;
+  int direction;
+} listIter;
+
+
+typedef struct list {
+  listNode* head;
+  listNode* tail;
+  void* (*dup)(void* ptr);
+  void (*free)(void* ptr);
+  int (*match)(void* ptr, void* key);
+  unsigned long len;
+} list;
+
+// Functions implemented as macros
+
+#define listLength(l) ((l)->len)
+#define listFirst(l) ((l)->head)
+#define listLast(l) ((l)->tail)
+#define listPrevNode(n) ((n)->prev)
+#define listNextNode(n) ((n)->next)
+#define listNodeValue(n) ((n)->value)
+
+#define listSetDupMethod(l, m) ((l)->dup = (m))
+#define listSetFreeMethod(l, m) ((l)->free = (m))
+#define listSetMatchMethod(l, m) ((l)->match = (m))
+
+#define listGetDupMethod(l) ((l)->dup)
+#define listGetFreeMethod(l) ((l)->free)
+#define listGetMatchMethod(l) ((l)->match)
+
+/**
+ * Prototypes
+ */
+list* listCreate(void);
+void listRelease(list* list);
+
+
+#endif // _ADLIST_H_
